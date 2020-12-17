19b67efc34d3bc1d865c3e3a89a408d246b6ccaa
shanjianqing <xiaosu3295@163.com>
2020-12-17 19:40:17 +0800

    20201217第二次更新

---
+++ 2.10.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+std::string global_str;
+int global_int;
+int main()
+{
+	int local_int;
+	std::string local_str;
+	std::cout << global_str << ", " << global_int
+		  << ", " << local_int << ", "
+		  << local_str << std::endl;
+	return 0;
+}
